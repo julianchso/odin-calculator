57d382cf384845c69f4271f7eb5354892669f16f
Julian So <ch.julian.so@gmail.com>
2023-01-26 21:58:29 -0500

    add removeLastDecimal function

--- index.js
+++ index.js
@@ -1,108 +1,114 @@
 const mainDisplay = document.querySelector("#mainDisplay");
-const formulaDisplay = document.querySelector("#formulaDisplay");
-
-const operators = document.querySelectorAll(".operator");
-// const multiply = document.querySelector("#multiply");
-// const divide = document.querySelector("#divide");
-// const subtract = document.querySelector("#subtract");
-// const add = document.querySelector("#add");
-
-const backspace = document.querySelector("#backspace");
-const clear = document.querySelector("#clear");
-const calculate = document.querySelector("#equals");
-
-const digits = document.querySelectorAll(".digit");
+const secondaryDisplay = document.querySelector("#secondaryDisplay");
+const operatorsBtn = document.querySelectorAll(".operator");
+const backspaceBtn = document.querySelector("#backspace");
+const clearBtn = document.querySelector("#clear");
+const calculateBtn = document.querySelector("#calculate");
+const digitsBtn = document.querySelectorAll(".digit");
 
 let firstNum = "";
 let secondNum = "";
 let operatorSign = "";
-
+let result = "";
 let firstNumToggle = true;
 
-clear.addEventListener("click", clearAll);
-calculate.addEventListener("click", equals);
-backspace.addEventListener("click", backspaceFn);
+clearBtn.addEventListener("click", clearAll);
+calculateBtn.addEventListener("click", calculate);
+backspaceBtn.addEventListener("click", backspaceFn);
 
 // Click first number and display it on main display.
-digits.forEach((digit) => {
-  digit.addEventListener("click", displayFirstValue);
-  // } else if (!firstNumToggle) {
+digitsBtn.forEach((digit) => {
+  digit.addEventListener("click", displayValue);
 });
 
-function displayFirstValue(e) {
-  if (operatorSign == "") {
-    firstNum += e.target.value;
-    mainDisplay.textContent = firstNum;
-    console.log(`displayFirstValue: ${firstNum}`);
-  } else if (operatorSign) {
-    secondNum += e.target.value;
-    mainDisplay.textContent = secondNum;
-    console.log(`displaySecondValue: ${secondNum}`);
-  }
-}
-
-operators.forEach((operator) => {
+operatorsBtn.forEach((operator) => {
   operator.addEventListener("click", operation);
 });
 
+// Store value in firstNum. If the operator sign is clicked, store value in secondNum.
+function displayValue(e) {
+  // while (firstNum.includes(".") {
+
+  // })
+  if (!operatorSign) {
+    firstNum += e.target.value;
+    // if (firstNum.includes(".")) return;
+    mainDisplay.textContent = firstNum;
+  } else if (operatorSign) {
+    // if (secondNum.includes(".")) return;
+    secondNum += e.target.value;
+    mainDisplay.textContent = secondNum;
+  }
+}
+
+// function appendDecimal(e) {
+//   if (firstNum)
+// }
+
+// Store operator sign and display on secondary display
 function operation(e) {
   operatorSign = e.target.textContent;
-  formulaDisplay.textContent = `${firstNum} ${operatorSign}`;
+  removeLastDecimal()
+  mainDisplay.textContent = `${firstNum}`;
+  secondaryDisplay.textContent = `${firstNum} ${operatorSign}`;
   firstNumToggle = false;
 }
 
-// Click second number. First number disappears from main display and second number appears.
-
-// If equals is clicked, add second number to formula display.
-
-// Display answer in the main display.
-
-function equals() {
+function calculate() {
   console.log(`firstNum: ${firstNum}`);
   console.log(`secondNum: ${secondNum}`);
-  formulaDisplay.textContent = `${firstNum} ${operatorSign} ${secondNum}`;
+  removeLastDecimal()
+  secondaryDisplay.textContent = `${firstNum} ${operatorSign} ${secondNum}`;
+  result = operate();
+  console.log(result);
+  mainDisplay.textContent = result;
 }
 
 function clearAll() {
   firstNum = "";
   secondNum = "";
-  formulaDisplay.textContent = "";
+  secondaryDisplay.textContent = "";
   mainDisplay.textContent = 0;
   operatorSign = "";
   firstNumToggle = true;
 }
 
 function backspaceFn() {
-  if (operatorSign == "") {
-    backspace.addEventListener("click", backspaceFirstValue);
-    console.log(`backspaceFn: ${firstNum}`);
-  } else {
-    backspace.addEventListener("click", backspaceSecondValue);
-    console.log(`secondNum: ${secondNum}`);
+  backspace.addEventListener("click", backspaceValue);
+}
+
+function backspaceValue() {
+  if (!operatorSign) {
+    firstNum = firstNum.slice(0, -1);
+    mainDisplay.textContent = firstNum;
+  } else if (operatorSign) {
+    secondNum = secondNum.slice(0, -1);
+    mainDisplay.textContent = secondNum;
   }
+  // console.log(`backspaceFirstValue: ${firstNum}`);
+  // console.log(firstNum);
 }
 
-function backspaceFirstValue() {
-  firstNum = firstNum.slice(0, firstNum.length - 1);
-  console.log(`backspaceFirstValue: ${firstNum}`);
-  console.log(firstNum);
-  mainDisplay.textContent = firstNum;
-}
+// function backspaceSecondValue() {
 
-function backspaceSecondValue() {
-  secondNum = secondNum.slice(0, secondNum.length - 1);
-  console.log(secondNum);
-  mainDisplay.textContent = secondNum;
-}
+// }
 
 function operate() {
-  if (multiply) {
+  firstNum = Number(firstNum);
+  secondNum = Number(secondNum);
+  if (operatorSign == "x") {
     return firstNum * secondNum;
-  } else if (divide) {
+  } else if (operatorSign == "÷") {
+    if (secondNum == 0) return "no no no";
     return firstNum / secondNum;
-  } else if (add) {
+  } else if (operatorSign == "+") {
     return firstNum + secondNum;
-  } else if (subtract) {
+  } else if (operatorSign == "-") {
     return firstNum - secondNum;
   }
+}
+
+function removeLastDecimal() {
+  if (firstNum[firstNum.length - 1] == ".") firstNum = firstNum.slice(0, -1);
+  if (secondNum[secondNum.length - 1] == ".") secondNum = secondNum.slice(0, -1);
 }
